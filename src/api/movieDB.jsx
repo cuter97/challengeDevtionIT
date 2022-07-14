543074bf09766eb6fb8eb8c28b2f7d7426dd7232
cuter97 <aguscastets@gmail.com>
2022-07-14 00:38:24 -0300

    add key

--- src/api/movieDB.jsx
+++ src/api/movieDB.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
-        api_key: process.env.REACT_APP_KEY,
+        api_key: `991884e1431b21661b4723ca4f451235`,
         language: 'en-US'
     }
 })
